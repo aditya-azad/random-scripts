c575db072c6f8e210dffbbfe85d8317c9c12f0b8
Aditya Azad <adityaazad121@gmail.com>
2020-08-16 03:58:29 +0530

    Bulk minimal clean tagging

--- mp3-tagger/tagger.py
+++ mp3-tagger/tagger.py
@@ -4,61 +4,84 @@ from mutagen.easyid3 import EasyID3
 import argparse
 import sys
 
+
+def get_all_files_in_dir(directory):
+    files = [args.file + x for x in os.listdir(args.file)]
+    return files
+
+
 def spaces_to_kebab_case(name):
     return name.replace(' ', '-').lower()
 
-def copy_file(filepath):
-    for retry in range(100):
-        try:
-            filename = os.path.basename(filepath)
-            outputpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "output", filename)
-            copyfile(filepath, outputpath)
-            return outputpath
-        except:
-            print("copying failed!")
-    sys.exit(1)
+
+def copy_file(directory, filepath):
+    try:
+        filename = os.path.basename(filepath)
+        outputpath = os.path.join(directory, filename)
+        copyfile(filepath, outputpath)
+        return outputpath
+    except Exception as e:
+        print("copying failed! ", e)
+        sys.exit(1)
+
 
 def rename_file(filepath):
-    for retry in range(100):
-        try:
-            # Get the info from audio file
-            audiofile = EasyID3(filepath)
-            currTitle = audiofile["title"]
-            currArtist = audiofile["artist"]
-            # Prepare the new name
-            filename = spaces_to_kebab_case("{0}-{1}.mp3".format(currTitle[0], currArtist[0]))
-            dirname = os.path.dirname(filepath)
-            # Rename
-            console.log(filename)
-            console.log(os.join(dirname, filename))
-            os.rename(filepath, os.join(dirname, filename))
-            break
-        except:
-            print("rename failed!")
-    sys.exit(1)
+    try:
+        # Get the info from audio file
+        audiofile = EasyID3(filepath)
+        currTitle = audiofile["title"]
+        currArtist = audiofile["artist"]
+        filename = os.path.basename(filepath)
+        # Prepare the new name
+        filename = spaces_to_kebab_case(
+            "{0}-{1}.mp3".format(currTitle[0], currArtist[0]))
+        dirname = os.path.dirname(filepath)
+        # Rename
+        os.rename(filepath, os.path.join(dirname, filename))
+        print("Renamed file: ", filename)
+    except Exception as e:
+        print("Rename failed! ", e)
+        sys.exit(1)
+
 
 def tag(filepath):
-    # Load file and store required information
-    audiofile = EasyID3(filepath)
-    currTitle = audiofile["title"]
-    currArtist = audiofile["artist"]
-    currAlbum = audiofile["album"]
-    # Remove tags
-    audiofile.delete()
-    # Clean tags
-    audiofile["title"] = currTitle
-    audiofile["artist"] = currArtist
-    audiofile["album"] = currAlbum
-    # Save file
-    audiofile.save()
+    try:
+        # Load file and store required information
+        audiofile = EasyID3(filepath)
+        currTitle = audiofile["title"]
+        currArtist = audiofile["artist"]
+        currAlbum = audiofile["album"]
+        # Remove tags
+        audiofile.delete()
+        # Clean tags
+        audiofile["title"] = currTitle
+        audiofile["artist"] = currArtist
+        audiofile["album"] = currAlbum
+        # Save file
+        audiofile.save()
+        return True
+    except:
+        print("ERROR TAGGING! ", os.path.basename(filepath))
+        copy_file(os.path.join(os.path.dirname(
+            os.path.realpath(__file__)), "error"), filepath)
+        return False
+
 
 # Parse args
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="specify the file you want to work with")
 args = parser.parse_args()
+# Program
 
-# The program
-filepath = copy_file(args.file)
-print(filepath)
-tag(filepath)
-rename_file(filepath)
+
+def main():
+    directory = get_all_files_in_dir(args.file)
+    for filepath in directory:
+        x = copy_file(os.path.join(os.path.dirname(
+            os.path.realpath(__file__)), "output"), filepath)
+        if tag(x):
+            rename_file(x)
+
+
+main()
+print("SUCCESS!")
